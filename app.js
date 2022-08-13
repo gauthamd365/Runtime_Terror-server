6582b18c84553e46a606c786aaf2a531734d25e2
gautham-d <102036884+gautham-d@users.noreply.github.com>
2022-08-13 15:39:07 +0530

    added local signin

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const passport = require("passport");
 
 const mongodbUri = process.env.MONGO_URI;
 
@@ -26,12 +27,22 @@ app.use(morgan("dev"));
 app.use(cors({ credentials: true, origin: "*" }));
 app.use(passport.initialize());
 
-
 app.get("/", (req, res) => {
-    res.send("Hello World");
-} );
- 
+  res.send("Hello World");
+});
 
+app.post(
+  "/auth/local/signin",
+  passport.authenticate("local", { session: false }),
+  function (req, res) {
+    console.log(req.user);
+    const token = jwt.sign({ user: req.user }, process.env.JWT_KEY, {
+      expiresIn: 60 * 60 * 24 * 1000,
+    });
+    
+    res.json({ user: req.user, token: token });
+  }
+);
 
 var port = process.env.PORT || 8000;
 
